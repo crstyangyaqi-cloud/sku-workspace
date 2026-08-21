f9b47fcd3f98051549cef4f4240ee438ab7bc5a2
SKU Workspace <952554068@qq.com>
2026-08-21 15:59:44 +0800

    Publish working mobile task creation

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sku-workspace-v1';
+const CACHE = 'sku-workspace-v3-mobile-create';
 const SHELL = ['/sku-workspace/', '/sku-workspace/manifest.webmanifest'];
 self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL))));
 self.addEventListener('activate', (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))));
